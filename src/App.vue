aa7a0ec7c84b514bd10183d71ee88451758f4ebe
ivanurra <ivan.urra.wd@gmail.com>
2021-07-15 15:31:20 +0200

    starting project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,5 @@
 <template>
-<h1>App Counter: {{counter}}</h1>
-<counter-one />
-<counter-action/>
+<h1>App</h1>
 </template>
 
-<script>
-import { provide, ref } from 'vue'
-import CounterOne from './components/CounterOne.vue'
-import CounterAction from './components/CounterAction.vue'
 
-export default {
-name: 'App',
-components: {
-    CounterOne,
-    CounterAction
-},
-setup(){
-    const counter = ref(0)
-
-    provide('counter', counter)
-
-    return { counter }
-}
-}
-</script>
-s
